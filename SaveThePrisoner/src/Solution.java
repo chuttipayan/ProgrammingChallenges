9d582bc2628d4fdb836261e8c476b7b1436b1651
Vikram Sridaran <Sunil.VikramSridaran@thomsonreuters.com>
2019-02-19 13:47:26 +0530

    Better Version

--- SaveThePrisoner/src/Solution.java
+++ SaveThePrisoner/src/Solution.java
@@ -1,97 +1,92 @@
 import java.io.*;
 
-
 import java.math.*;
 import java.security.*;
 import java.text.*;
 import java.util.*;
 import java.util.concurrent.*;
 import java.util.regex.*;
+
 /**
  * 
- * A jail has a number of prisoners and a number of treats to pass out to them. Their jailer decides the fairest way to divide the treats is to seat the prisoners around a circular table in sequentially numbered chairs. A chair number will be drawn from a hat. Beginning with the prisoner in that chair, one candy will be handed to each prisoner sequentially around the table until all have been distributed.
-
-The jailer is playing a little joke, though. The last piece of candy looks like all the others, but it tastes awful. Determine the chair number occupied by the prisoner who will receive that candy.
-
-For example, there are  prisoners and  pieces of candy. The prisoners arrange themselves in seats numbered  to . Let's suppose two is drawn from the hat. Prisoners receive candy at positions . The prisoner to be warned sits in chair number .
-
-Function Description
-
-Complete the saveThePrisoner function in the editor below. It should return an integer representing the chair number of the prisoner to warn.
-
-saveThePrisoner has the following parameter(s):
-
-n: an integer, the number of prisoners
-m: an integer, the number of sweets
-s: an integer, the chair number to begin passing out sweets from
-Input Format
-
-The first line contains an integer, , denoting the number of test cases. 
-The next  lines each contain  space-separated integers: 
-- : the number of prisoners 
-- : the number of sweets 
-- : the chair number to start passing out treats at
-
-Constraints
-
-Output Format
-
-For each test case, print the chair number of the prisoner who receives the awful treat on a new line.
+ * A jail has a number of prisoners and a number of treats to pass out to them.
+ * Their jailer decides the fairest way to divide the treats is to seat the
+ * prisoners around a circular table in sequentially numbered chairs. A chair
+ * number will be drawn from a hat. Beginning with the prisoner in that chair,
+ * one candy will be handed to each prisoner sequentially around the table until
+ * all have been distributed.
+ * 
+ * The jailer is playing a little joke, though. The last piece of candy looks
+ * like all the others, but it tastes awful. Determine the chair number occupied
+ * by the prisoner who will receive that candy.
+ * 
+ * For example, there are prisoners and pieces of candy. The prisoners arrange
+ * themselves in seats numbered to . Let's suppose two is drawn from the hat.
+ * Prisoners receive candy at positions . The prisoner to be warned sits in
+ * chair number .
+ * 
+ * Function Description
+ * 
+ * Complete the saveThePrisoner function in the editor below. It should return
+ * an integer representing the chair number of the prisoner to warn.
+ * 
+ * saveThePrisoner has the following parameter(s):
+ * 
+ * n: an integer, the number of prisoners m: an integer, the number of sweets s:
+ * an integer, the chair number to begin passing out sweets from Input Format
+ * 
+ * The first line contains an integer, , denoting the number of test cases. The
+ * next lines each contain space-separated integers: - : the number of prisoners
+ * - : the number of sweets - : the chair number to start passing out treats at
+ * 
+ * Constraints
+ * 
+ * Output Format
+ * 
+ * For each test case, print the chair number of the prisoner who receives the
+ * awful treat on a new line.
  */
 public class Solution {
 
-    // Complete the saveThePrisoner function below.
-    static int saveThePrisoner(int n, int m, int s) {
-    	int lastPosition=0;
-    	while(m!=0 )
-    	{
-    		
-    		if(s+m>n)
-    		{
-    			lastPosition= n;
-    			m= m-(n-s+1);
-    			
-    			s=1;
-    		}
-    		else
-    		{
-    			
-    			s=s+m-1;
-    			lastPosition=s;
-    			m=0;
-    		}
-    		
-    		
-    	}
-    	
-		
-    	
-    	return lastPosition;
+	// Complete the saveThePrisoner function below.
+	static int saveThePrisoner(int n, int m, int s) {
+		int lastPosition = 0;
 
+		if (s + m > n) {
+			m = m - (n - s + 1);
+			lastPosition = m % n;
+			 if(lastPosition==0)
+	                lastPosition=n;
 
-    }
+		} else {
+			s = s + m - 1;
+			lastPosition = s;
+		}
 
-    private static final Scanner scanner = new Scanner(System.in);
+		return lastPosition;
 
-    public static void main(String[] args) throws IOException {
-        int t = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+	}
 
-        for (int tItr = 0; tItr < t; tItr++) {
-            String[] nms = scanner.nextLine().split(" ");
+	private static final Scanner scanner = new Scanner(System.in);
 
-            int n = Integer.parseInt(nms[0]);
+	public static void main(String[] args) throws IOException {
+		int t = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-            int m = Integer.parseInt(nms[1]);
+		for (int tItr = 0; tItr < t; tItr++) {
+			String[] nms = scanner.nextLine().split(" ");
 
-            int s = Integer.parseInt(nms[2]);
+			int n = Integer.parseInt(nms[0]);
 
-            int result = saveThePrisoner(n, m, s);
-            System.out.println(result);
-           
-        }
+			int m = Integer.parseInt(nms[1]);
 
-        
-        scanner.close();
-    }
+			int s = Integer.parseInt(nms[2]);
+
+			int result = saveThePrisoner(n, m, s);
+			System.out.println(result);
+
+		}
+
+		scanner.close();
+	}
 }
