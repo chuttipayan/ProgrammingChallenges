ad9975c1ad021cfbda14fdc45fc20c4b323de027
chuttipayan <sunilvikram@gmail.com>
2019-02-21 15:29:02 +0530

    template code

--- strangecounter/src/main/java/com/sunil/prgchallenges/strangecounter/App.java
+++ strangecounter/src/main/java/com/sunil/prgchallenges/strangecounter/App.java
@@ -6,7 +6,24 @@ import java.io.IOException;
 import java.util.Scanner;
 
 /**
- * Hello world!
+ *Bob has a strange counter. At the first second, it displays the number . Each second, the number displayed by the counter decrements by  until it reaches .
+
+The counter counts down in cycles. In next second, the timer resets to  and continues counting down. The diagram below shows the counter values for each time  in the first three cycles:
+
+https://s3.amazonaws.com/hr-challenge-images/22185/1469447349-bae87a5071-strange1.png
+
+Find and print the value displayed by the counter at time .
+
+Function Description
+
+Complete the strangeCounter function in the editor below. It should return the integer value displayed by the counter at time .
+
+strangeCounter has the following parameter(s):
+
+t: an integer
+Input Format
+
+A single integer denoting the value of  t.
  *
  */
 public class App 
