132aa1fc4c557199fb8749a1f83bfbbb4e10f503
chuttipayan <sunilvikram@gmail.com>
2019-02-21 15:26:26 +0530

    template code

--- strangecounter/src/main/java/com/sunil/prgchallenges/strangecounter/App.java
+++ strangecounter/src/main/java/com/sunil/prgchallenges/strangecounter/App.java
@@ -1,13 +1,38 @@
 package com.sunil.prgchallenges.strangecounter;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	
+	// Complete the strangeCounter function below.
+    static long strangeCounter(long t) {
+    	return 0;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long t = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = strangeCounter(t);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
