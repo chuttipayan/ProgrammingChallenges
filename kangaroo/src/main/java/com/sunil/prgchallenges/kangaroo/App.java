b8135c7beb6108336dc7fb47d57cc62dd4e015cd
chuttipayan <sunilvikram@gmail.com>
2019-02-28 16:04:38 +0530

    New Solution

--- kangaroo/src/main/java/com/sunil/prgchallenges/kangaroo/App.java
+++ kangaroo/src/main/java/com/sunil/prgchallenges/kangaroo/App.java
@@ -1,13 +1,51 @@
 package com.sunil.prgchallenges.kangaroo;
 
 /**
- * Hello world!
+ * Refer to the https://www.hackerrank.com/challenges/kangaroo/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class App {
+
+    // Complete the kangaroo function below.
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+    	if(x2>= x1 && v2 > v1)
+    		return "NO";
+    	if(x1>=x2 && v1 >v2)
+    		return "NO";
+
+    	return "YES";
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] x1V1X2V2 = scanner.nextLine().split(" ");
+
+        int x1 = Integer.parseInt(x1V1X2V2[0]);
+
+        int v1 = Integer.parseInt(x1V1X2V2[1]);
+
+        int x2 = Integer.parseInt(x1V1X2V2[2]);
+
+        int v2 = Integer.parseInt(x1V1X2V2[3]);
+
+        String result = kangaroo(x1, v1, x2, v2);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
     }
 }
