9e9219695e343e6b04ce94b47c6290f47afaf363
chuttipayan <sunilvikram@gmail.com>
2019-03-28 12:52:16 +0530

    final solution

--- sherlocksquares/src/main/java/com/sunil/prgchallenges/sherlocksquares/App.java
+++ sherlocksquares/src/main/java/com/sunil/prgchallenges/sherlocksquares/App.java
@@ -1,13 +1,45 @@
 package com.sunil.prgchallenges.sherlocksquares;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
 /**
- * Hello world!
+ * 
+ * https://www.hackerrank.com/challenges/sherlock-and-squares/problem
+ * @author chuttipayan
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+
+public class App {
+
+    // Complete the squares function below.
+    static int squares(int a, int b) {
+    	int num=0;
+        num=(int)(Math.floor(Math.sqrt(b)))-(int)(Math.ceil(Math.sqrt(a)))+1;
+        return num;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+       
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int qItr = 0; qItr < q; qItr++) {
+            String[] ab = scanner.nextLine().split(" ");
+            
+            int a = Integer.parseInt(ab[0]);
+
+            int b = Integer.parseInt(ab[1]);
+
+            int result = squares(a, b);
+            System.out.println(result);
+
+        }
+
+       
+        scanner.close();
     }
 }
