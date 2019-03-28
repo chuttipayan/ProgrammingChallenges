29d4f97987f118ac67d6da7c15ab7b2a61a704a1
chuttipayan <sunilvikram@gmail.com>
2019-03-28 12:22:51 +0530

    initial project

---
+++ sherlocksquares/src/main/java/com/sunil/prgchallenges/sherlocksquares/App.java
@@ -0,0 +1,13 @@
+package com.sunil.prgchallenges.sherlocksquares;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
